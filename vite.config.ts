19f527b3a62cc362963b3d39d900ea5eee5e1029
kzha0 <>
2025-01-06 01:27:26 +0800

    add netlify plugin to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import mdx from "@mdx-js/rollup";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 declare module "@remix-run/node" {
     interface Future {
@@ -25,6 +26,7 @@ export default defineConfig({
             appDirectory: "./src/app",
         }),
         tsconfigPaths(),
+        netlifyPlugin(),
     ],
     resolve: {
         alias: {
